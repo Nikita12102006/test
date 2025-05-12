57706773fc58be4392d36ae7e7825b56a77da51b
Nikita <gusenkovnikita@yandex.ru>
2025-05-12 23:59:31 +0300

    One

---
+++ Cicl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cicl
+{
+    internal class Program
+    {
+        public static int sum(int a, int b)
+        {
+            Debug.WriteLine($"{a} {b}");
+            var file = new TextWriterTraceListener("test.txt");
+            Trace.Listeners.Add(file);
+            Trace.AutoFlush = true;
+            Trace.WriteLine($"{a} {b}");
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            sum(4, 5);
+        }
+    }
+}
